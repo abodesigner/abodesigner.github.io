cd754df53c562a240f0a0a01a8a5e67830663ef6
abodesigner <mohammed.abosalim@gmail.com>
2019-11-17 01:13:46 +0200

    first update

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
 // A $( document ).ready() block.
 $(document).ready(function() {
-
   // hidden collapse menu in small screen
-  $('.nav li a').click(function(event) {
+  $(".nav li a").click(function(event) {
     // only do this if navigation is visible, otherwise you see jump in navigation while collapse() is called
-    if ($(".navbar-collapse").is(":visible") && $(".navbar-toggle").is(":visible") ) {
-      $('.navbar-collapse').collapse('toggle');
+    if (
+      $(".navbar-collapse").is(":visible") &&
+      $(".navbar-toggle").is(":visible")
+    ) {
+      $(".navbar-collapse").collapse("toggle");
     }
   });
 
+  // Animate My Projects Section
+  var mixer = mixitup("#Container");
 
-
-    // Animate My Projects Section
-    var mixer = mixitup('#Container');
-
-
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-
+  // Add smooth scrolling to all links
+  $("a").on("click", function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -28,31 +26,31 @@ $(document).ready(function() {
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
     } // End if
-
-    });
-
+  });
 
   //hide placeholder on focus
   var $place;
-  $(':input').on('focus', function(){
-      $place = $(this).attr("placeholder");
-      $(this).attr("placeholder","");
+  $(":input").on("focus", function() {
+    $place = $(this).attr("placeholder");
+    $(this).attr("placeholder", "");
   });
 
   //blur placeholder on focus
-  $(':input').on('blur', function(){
-      $(this).attr("placeholder",$place);
+  $(":input").on("blur", function() {
+    $(this).attr("placeholder", $place);
   });
 
-
-
-
+  // Get the full year
+  $("#year").text(new Date().getFullYear());
 });
