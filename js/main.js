189bd85a6c73f43dd7d88ca98f4809db5818d44d
abodesigner <mohammed.abosalim@gmail.com>
2022-05-28 15:41:39 +0200

    chaning the about me section

--- js/main.js
+++ js/main.js
@@ -1,43 +1,9 @@
 // A $( document ).ready() block.
 $(document).ready(function () {
-  // hidden collapse menu in small screen
-  $(".nav li a").click(function (event) {
-    // only do this if navigation is visible, otherwise you see jump in navigation while collapse() is called
-    if (
-      $(".navbar-collapse").is(":visible") &&
-      $(".navbar-toggle").is(":visible")
-    ) {
-      $(".navbar-collapse").collapse("toggle");
-    }
-  });
 
-  // Animate My Projects Section
-  //var mixer = mixitup("#Container");
 
-  // Add smooth scrolling to all links
-  $("a").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
-        },
-        800,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
 
   //hide placeholder on focus
   var $place;
